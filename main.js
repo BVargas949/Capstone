550f7ebdd0eb434133f1e4b932141683bd076e5f
Evelyn Duarte <eduarte019@west-mec.org>
2019-05-06 18:22:22 -0700

    Update 05/06
    Added html for images, css for styles to the images, javaScript for the slideshow

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 // window.onload = function () {
 //     alert("welcome to the Avionics/ Drone Electronics page!")
 // }
+
+// slideshow
+var slideNum = 0;
+show();
+
+function show() {
+    var i;
+    var slides = document.getElementsByClassName("slides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideNum++;
+    if (slideNum > slides.length) {
+        slideNum = 1
+    }
+    slides[slideNum - 1].style.display = "block";
+    setTimeout(show, 4000);
+}
